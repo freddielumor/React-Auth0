959b7e9bb30279575e77866f8b9f71a40ba24ef6
Freddie Lumor <freddielumor@gmail.com>
2020-12-30 15:57:01 +0000

    Updates
    - Add auth logic to signup, login, logout components
    - Update profile view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
-import { NavBar } from "./components";
+import { NavBar, Loading } from "./components";
 import { Home, Profile, ExternalApi } from "./views";
 
 import "./App.css";
 
 const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <Loading />;
+
   return (
     <div id="app" className="d-flex flex-column h-100">
       <NavBar />
